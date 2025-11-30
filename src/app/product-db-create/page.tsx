4ec95fc1c636545518f54767431deef30f26c416
yue <2896677581@qq.com>
2025-11-30 16:24:36 +0800

    feat: 使用react内置的形式处理表单信息学数据

---
+++ src/app/product-db-create/page.tsx
@@ -0,0 +1,39 @@
+import { addProduct } from '@/prisma-db';
+import { redirect } from 'next/navigation';
+
+export default function () {
+  const createProduct = async (formData: FormData) => {
+    'use server';
+    const title = formData.get('title') as string;
+    const price = formData.get('price') as string;
+    const description = formData.get('description') as string;
+
+    await addProduct({ title, price: parseInt(price), description });
+
+    redirect('/product-db');
+  };
+
+  return (
+    <>
+      <form action={createProduct}>
+        <label>
+          title: <input type="text" name="title" />
+        </label>
+        <br />
+
+        <label>
+          price: <input type="text" name="price" />
+        </label>
+        <br />
+
+        <label>
+          description:
+          <input type="text" name="description" />
+        </label>
+
+        <br />
+        <button type="submit">add</button>
+      </form>
+    </>
+  );
+}
